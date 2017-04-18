7e2e308de765d4d45fbff148f6d98743afa9e9c2
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-04-18 23:20:51 +0200

    added little doc

--- test_compare.py
+++ test_compare.py
@@ -14,9 +14,10 @@ data2 = numpy.random.randn(n_pairs, sz, 1).astype(numpy.float)
 t0 = time.time()
 for i in range(n_pairs):
     d = dtw.dtw_sq(data1[i], data2[i])
-print(time.time() - t0)
+print("Cython computation takes %f seconds for %d DTW of length %d (dimension %d)" % (time.time() - t0, n_pairs, sz, 1))
 
 t0 = time.time()
 for i in range(n_pairs):
     d = dtw_ref.dtw_sq(data1[i], data2[i])
-print(time.time() - t0)
+print("Pure python computation takes %f seconds for %d DTW of length %d (dimension %d)" % (time.time() - t0, n_pairs,
+                                                                                           sz, 1))
