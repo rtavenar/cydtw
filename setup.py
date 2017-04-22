e027be4f27bbf286caa451ce61dc5606189d5654
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-04-23 00:20:01 +0200

    PyPI-ready

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    name='cydtw',
+    name="cydtw",
+    description = "DTW computation made efficient using Cython",
     ext_modules=cythonize("cydtw.pyx"),
     include_dirs=[numpy.get_include()],
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/rtavenar/cydtw",
     author="Romain Tavenard",
     author_email="romain.tavenard@univ-rennes2.fr"
