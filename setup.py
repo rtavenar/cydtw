820199a29912d545cfa65c8397c97b9bf5ae1e66
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-04-24 12:36:02 +0200

    Specifying dependencies in setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
@@ -7,7 +7,8 @@ setup(
     description = "DTW computation made efficient using Cython",
     ext_modules=cythonize("cydtw.pyx"),
     include_dirs=[numpy.get_include()],
-    version="0.1.3",
+    install_requires=['Cython', 'numpy', 'scipy'],
+    version="0.1.3.2",
     url="https://github.com/rtavenar/cydtw",
     author="Romain Tavenard",
     author_email="romain.tavenard@univ-rennes2.fr"
